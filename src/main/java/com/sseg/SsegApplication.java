be797f6436ce21ec388331364cfe8f0f2c09e693
sechan100 <sechan100@gmail.com>
2023-09-25 10:33:19 +0900

    sseg init

---
+++ src/main/java/com/sseg/SsegApplication.java
@@ -0,0 +1,13 @@
+package com.sseg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sseg/SsegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sseg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
