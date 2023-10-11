0bd1d23d1e57919145546f7cb55f3979bbc558e9
sechan100 <sechan100@gmail.com>
2023-10-12 06:03:20 +0900

    fix: 템플릿 이름을 찾을 수 없는 경우 발생하는 에러응답 수정
    템플릿 이름을 기반으로 템플릿을 찾지 못했을 경우, TEMPLATE_NOT_FOUND 상태가 전송되어야하는데, 다른 예외가 발생하고 있던걸 수정
    
    Optionl을 미리 꺼내온다음 일괄적으로 String.valueOf 했던게 문제였다. null이 "null"로 들어가던 것.

--- src/main/java/io/sseg/boundedcontext/email/controller/ApiEmailController.java
+++ src/main/java/io/sseg/boundedcontext/email/controller/ApiEmailController.java
@@ -8,20 +8,17 @@ import io.sseg.boundedcontext.application.entity.Application;
 import io.sseg.boundedcontext.application.entity.EmailTemplate;
 import io.sseg.boundedcontext.application.exception.TemplateParsingException;
 import io.sseg.boundedcontext.application.service.ApplicationService;
-import io.sseg.boundedcontext.email.EmailEnums;
-import io.sseg.boundedcontext.email.constants.SendingProcessStatus;
 import io.sseg.boundedcontext.email.model.ApiEmailRequest;
-import io.sseg.boundedcontext.email.model.EmailRequest;
-import io.sseg.boundedcontext.email.model.EmailResponse;
 import io.sseg.boundedcontext.email.service.EmailSendService;
 import io.sseg.boundedcontext.email.service.ThymeleafEmailTemplateResolver;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -44,15 +41,15 @@ public class ApiEmailController {
         Application application = applicationService.findByAppId(appId);
         
         // 이메일 전송자 이름을 '요청 받은 전송자 이름@도메인' 형식으로 변환
-        request.getEmailRequest().setFrom(request.getEmailRequest().getFrom() + application.getDomain());
+        request.getEmailRequest().setFrom(request.getEmailRequest().getFrom() + "@" + application.getDomain());
         
         // 사용자 애플리케이션의 템플릿 목록에서 요청에 담긴 템플릿 이름으로 템플릿을 찾는다.
-        String template = String.valueOf(application.getEmailTemplates()
+        String template = application.getEmailTemplates()
                 .stream()
                 .filter(t -> t.getName().equals(request.getTemplateName()))
                 .map(EmailTemplate::getTemplate)
-                .findAny().orElse(null)
-        );
+                .findAny().orElse(null);
+        
         
         // error: 요청 받은 템플릿 이름으로 템플릿을 찾지 못했을 경우
         if(template == null) {
@@ -82,10 +79,14 @@ public class ApiEmailController {
             return ApiResponse.unprocessableEntity(SsegApiResponseStatus.INTERNAL_SERVER_ERROR);
         }
 
-        // 이메일 요청정보와 파싱된 템플릿, 애플리케이션에 등록된 smtp 서버 정보를 사용하여 이메일을 전송
-        emailService.sendMail(request.getEmailRequest(), template, application.getSmtpProperties());
-        
-        return ApiResponse.ok(SsegApiResponseStatus.SUCCESS);
+        try {
+            // 이메일 요청정보와 파싱된 템플릿, 애플리케이션에 등록된 smtp 서버 정보를 사용하여 이메일을 전송
+            emailService.sendMail(request.getEmailRequest(), template, application.getSmtpProperties());
+            return ApiResponse.ok(SsegApiResponseStatus.SUCCESS);
+            
+        } catch(MessagingException | MailException e){
+            return ApiResponse.unprocessableEntity(SsegApiResponseStatus.INTERNAL_SERVER_ERROR, "internal server error msg => '" + e.getMessage() +"'");
+        }
         
     }
     
